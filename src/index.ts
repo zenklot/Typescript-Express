025342cfce20f6e62b8e89982854cf17445d1c1c
zenklot <zenklot11@gmail.com>
2022-12-30 21:05:34 +0700

    new route users with post, add bodyParser but Unused :(

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response} from 'express';
+import bodyParser from 'body-parser'
 
 class App {
     public app: Application;
@@ -6,13 +7,24 @@ class App {
     constructor() {
         this.app = express();
         this.app.use(express.json());
+        // this.middleware()
         this.routes()
+    }
+
+    protected middleware() : void {
+        this.app.use(bodyParser.json());
     }
 
     protected routes(): void {
         this.app.route('/').get((req: Request, res: Response) => {
             res.json({
                 message: 'Express With Typescript'
+            })
+        })
+
+        this.app.route('/users').post((req: Request, res: Response) => {
+            res.json({
+                req: req.body
             })
         })
     }
