6302d7bbe2d27d908f3b262c6a89843655aa927b
zenklot <zenklot11@gmail.com>
2022-12-30 21:56:27 +0700

    add logic controller with dummy data

--- src/routers/UserRoutes.ts
+++ src/routers/UserRoutes.ts
@@ -14,6 +14,9 @@ class UserRoutes implements IRouter {
     routes(): void {
         this.router.get('/', UserController.index)
         this.router.post('/', UserController.create)
+        this.router.get('/:id', UserController.show)
+        this.router.put('/:id', UserController.update)
+        this.router.delete('/:id', UserController.destroy)
     }
 }
 
