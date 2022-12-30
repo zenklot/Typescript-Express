ecd5b4489b4832d0ebac2f248c0c00bd28c1645b
zenklot <zenklot11@gmail.com>
2022-12-30 21:16:31 +0700

    add dependencies compression, helmet, cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response} from 'express';
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
+import compression from 'compression'
+import helmet from 'helmet'
+import cors from 'cors'
 
 class App {
     public app: Application;
@@ -15,6 +18,9 @@ class App {
     protected middleware() : void {
         // this.app.use(bodyParser.json());
         this.app.use(morgan('dev'));
+        this.app.use(compression());
+        this.app.use(helmet());
+        this.app.use(cors());
     }
 
     protected routes(): void {
