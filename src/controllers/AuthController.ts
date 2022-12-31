ffb9c15fb72778187fa42a25937cf2264ad768bc
zenklot <zenklot11@gmail.com>
2022-12-31 10:40:23 +0700

    Login controller

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express'
 const db = require("../db/models")
-import PasswordHash from '../utils/PasswordHash';
+import Authentication from '../utils/Authentication';
 
 class AuthController {
     register = async (req: Request, res: Response): Promise<Response> => {
         let { username, password } = req.body;
-        const hashPassword: string = await PasswordHash.hash(password)
+        const hashPassword: string = await Authentication.PasswordHash(password)
         const createdUser = await db.user.create({
             username, password: hashPassword
         });
@@ -14,11 +14,29 @@ class AuthController {
 
         
     }
-    login(req: Request, res: Response): Response {
-        const { id, name } = req.body;
-        return res.json({
-            message: "cek"
-        })
+    login = async (req: Request, res: Response): Promise<Response> => {
+        let { username, password } = req.body;
+        // Cari data user dari username
+        const user = await db.user.findOne({
+            where: {username}
+        });
+
+        // cek password
+        let compare = await Authentication.PasswordCompare(password, user.password);
+        if (!user || !compare) {
+            return res.status(401).json({
+                message: "Username or password incorrect"
+            });
+        }
+
+        // generate token
+        const token = Authentication.GenerateToken(user.id, username, user.password);
+        return res.status(200).json({
+            staus: 'success',
+            data: {
+                access_token: token
+            }
+        });
     }
 
 }
--- src/utils/PasswordHash.ts
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcrypt';
-
-class PasswordHash {
-    public static hash = (password:string) : Promise<string> => bcrypt.hash(password, 10)
-}
-
-export default PasswordHash;
