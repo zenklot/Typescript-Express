6d499fd2c2aa57cbbd88c88a7e609e8f79dcb6db
zenklot <zenklot11@gmail.com>
2022-12-31 12:20:57 +0700

    Middleware jwt verify

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -1,14 +1,29 @@
 import { Request, Response, NextFunction } from "express";
-
+import jwt from 'jsonwebtoken'
 export const auth = (req: Request, res: Response, next: NextFunction) : any => {
-    let auth = false;
+    if(!req.headers.authorization){
+        return res.status(401).json({
+            status: 401,
+            message: 'Unauthorized'
+        });
+    }
+    let secretKey = process.env.JWT_SECRET_KEY || 'secret'
+    const token:string = req.headers.authorization.split(" ")[1]
 
-    if(auth){
-        next();
-    }else{
-        return res.json({
-            success: false,
-            message: "Unauthorized"
+    try{
+        const credential : string | jwt.JwtPayload = jwt.verify(token, secretKey)
+        if(credential) {
+            req.app.locals.credential = credential
+            next();
+        }
+        return res.status(401).json({
+            status: 401,
+            message: 'Invalid token'
+        })
+    }catch(err){
+        return res.status(401).json({
+            status: 401,
+            message: err
         })
     }
 
