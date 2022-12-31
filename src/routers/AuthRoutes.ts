763da4f3d806550127857541ac6431ec88225dec
zenklot <zenklot11@gmail.com>
2022-12-31 10:01:11 +0700

    Add validator for auth

--- src/routers/AuthRoutes.ts
+++ src/routers/AuthRoutes.ts
@@ -1,11 +1,12 @@
 import BaseRoutes from './BaseRouter';
 import AuthController from '../controllers/AuthController';
-
+// Validators
+import validate from '../middleware/AuthValidator';
 
 class AuthRoutes extends BaseRoutes {
     routes(): void {
-        this.router.post('/register', AuthController.register)
-        this.router.post('/login', AuthController.login)
+        this.router.post('/register', validate, AuthController.register)
+        this.router.post('/login', validate, AuthController.login)
     }
 }
 
